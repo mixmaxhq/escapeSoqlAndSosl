46ea2a7fea2281770810bf65f16781e6f4afa47f
Randall Ma <randall@randallma.com>
2017-06-16 11:46:01 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # escapeSoqlAndSosl
-escape SOQL queries and SOSL searches
+
+Escape Salesforce SOQL queries and SOSL searches. Can be used standalone or in conjunction with JSForce.
+
+### Installation
+
+```
+> npm install --save-dev escapeSoqlAndSosl
+```
+
+### Usage
+
+```js
+const {escapeSOQL, escapeSOSL} = require('escapeSoqlAndSosl');
+const jsforce = require('jsforce');
+
+const conn = ...
+
+const query = `FIND {@${escapeSOSL('pride-feat-dangelo.com')}} IN email FIELDS RETURNING Contact(Id, AccountId WHERE AccountId != null)`;
+conn.search(query);
+```
+
+### License
+
+The [MIT License](LICENSE).
---
+++ index.js
@@ -0,0 +1,27 @@
+module.exports = {
+  /**
+   * Escapes the following characters with backslashes (for use in SOQL QUERY queries):
+   * ' \
+   *
+   * See SFDC API docs for more details:
+   * https://developer.salesforce.com/docs/atlas.en-us.soql_sosl.meta/soql_sosl/sforce_api_calls_soql_select_reservedcharacters.htm
+   *
+   * @param {String} str - search query to escape
+   *
+   * @returns {String} escapedString
+   */
+  escapeSOQL: (str) => str.replace(/(['\\])/g, '\\$&'),
+
+  /**
+   * Escapes the following characters with backslashes (for use in SOSL FIND queries):
+   * ? & | ! { } [ ] ( ) ^ ~ * : \ " ' + -
+   *
+   * See SFDC API docs for more details:
+   * https://developer.salesforce.com/docs/atlas.en-us.soql_sosl.meta/soql_sosl/sforce_api_calls_sosl_find.htm
+   *
+   * @param {String} str - search query to escape
+   *
+   * @returns {String} escapedString
+   */
+  escapeSOSL: (str) => str.replace(/([?&|!{}[\]()^~*:\\"'+-])/g, '\\$&')
+};
